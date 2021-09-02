1791166beacdc0385eebc92ea59762aedff02763
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-09-02 16:05:53 +0530

    Add project files.

---
+++ ConsoleAppMultilevelInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MultilevelInheritance
+{
+    class Shape
+    {
+        public void Display()
+        {
+            Console.WriteLine("Shape is:");
+        }
+    }
+    class Rectangle : Shape
+    {
+        public void PrintRectangle()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+    class Square : Rectangle
+    {
+        public void PrintSquare()
+        {
+            base.Display();
+            Console.WriteLine("Square");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            square.PrintSquare();
+            square.Display();
+            square.PrintRectangle();
+            Console.ReadKey();
+        }
+    }
+}
